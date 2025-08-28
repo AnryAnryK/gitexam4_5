2f7d2a65ca2e8f89920b386ec9ff528d57981b04
AnryAnryK <andkay@yandex.ru>
2025-08-28 11:02:41 +0300

    Изменена функция сложения

--- App/App.java
+++ App/App.java
@@ -1,18 +1,11 @@
 package ru.yandex.calculator
 
 public class Calculator {
-	float a;
-    float b;
-    float c;
-    float d;
-    float e;
+	
 	
     public static void main(String[] args) {
 
     }
 	
-	float multiplication3Numbers(float a, float b, float c) {
-        float d = a * b * c;
-        return d;
-    }
+	//multiplication 3 arguments
 }
