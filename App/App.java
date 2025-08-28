95ccdc754516b06adae731c90ce7193b10ce6268
AnryAnryK <andkay@yandex.ru>
2025-08-28 10:40:17 +0300

    Добавлена функция умножения

--- App/App.java
+++ App/App.java
@@ -1,7 +1,18 @@
 package ru.yandex.calculator
 
 public class Calculator {
+	float a;
+    float b;
+    float c;
+    float d;
+    float e;
+	
     public static void main(String[] args) {
 
+    }
+	
+	float multiplication3Numbers(float a, float b, float c) {
+        float d = a * b * c;
+        return d;
     }
 }
